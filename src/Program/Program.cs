bec2a8d6b531a492a827da978fbe8738ff656812
Matias Ubbriaco <mhubb923@gmail.com>
2026-08-05 19:33:21 -0300

    Comentario agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,6 +7,7 @@
         Console.WriteLine(Multiplication.Multiply(5, 6));
         Console.WriteLine(Division.Divide(7, 8));
     }
+    // Esta clase implementa la operación resta
     public class Subtraction
 {
    public static int Subtract(int a, int b)
