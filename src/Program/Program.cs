c5c48f1bbba5bd15d519a610002ad041bbf069b6
Matias Ubbriaco <mhubb923@gmail.com>
2026-08-05 19:25:57 -0300

    Implementación de la operación resta

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,4 +7,11 @@
         Console.WriteLine(Multiplication.Multiply(5, 6));
         Console.WriteLine(Division.Divide(7, 8));
     }
+    public class Subtraction
+{
+   public static int Subtract(int a, int b)
+   {
+       return a - b;
+   }
+}
 }
